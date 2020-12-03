d07b437ea6f49e88e404be7106e1edd3fa60c954
NanRos <nancyrosa956@gmail.com>
2020-12-02 23:34:12 -0800

    Changed app styles to NoActionBar, Included Toolbar to MainActivity

--- app/src/main/java/com/example/fandome/MainActivity.java
+++ app/src/main/java/com/example/fandome/MainActivity.java
@@ -1,8 +1,8 @@
 package com.example.fandome;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
@@ -21,15 +21,17 @@ public class MainActivity extends AppCompatActivity {
     public static final String TAG="MainActivity";
     final FragmentManager fragmentManager = getSupportFragmentManager();
     private BottomNavigationView bottomNavigationView;
+    //private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
-        getSupportActionBar().setCustomView(R.layout.actionbar_title);
+        //toolbar = findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
 
         setContentView(R.layout.activity_main);
+
 
         bottomNavigationView = findViewById(R.id.bottomNavigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener(){
@@ -40,16 +42,13 @@ public class MainActivity extends AppCompatActivity {
                 switch (menuItem.getItemId()) {
                     case R.id.action_home:
                         fragment = new HomeFragment();
-                        Toast.makeText(MainActivity.this, "HOME", Toast.LENGTH_SHORT).show();
                         break;
                     case R.id.action_search:
                         fragment = new SearchFragment();
-                        Toast.makeText(MainActivity.this, "SEARCH", Toast.LENGTH_SHORT).show();
                         break;
                     case R.id.action_profile:
                     default:
                         fragment = new ProfileFragment();
-                        Toast.makeText(MainActivity.this, "PROFILE", Toast.LENGTH_SHORT).show();
                         break;
                 }
                 fragmentManager.beginTransaction().replace(R.id.fragmentContainer, fragment).commit();
