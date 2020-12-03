c61ae3f23f928e21b23ea6ac6bfb8aeb60554a05
NanRos <nancyrosa956@gmail.com>
2020-12-02 19:06:31 -0800

    Added Bottom Navigation Functionality

--- app/src/main/java/com/example/fandome/MainActivity.java
+++ app/src/main/java/com/example/fandome/MainActivity.java
@@ -1,14 +1,62 @@
 package com.example.fandome;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.example.fandome.fragments.HomeFragment;
+import com.example.fandome.fragments.ProfileFragment;
+import com.example.fandome.fragments.SearchFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG="MainActivity";
+    final FragmentManager fragmentManager = getSupportFragmentManager();
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setCustomView(R.layout.actionbar_title);
+
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener(){
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                Fragment fragment;
+                switch (menuItem.getItemId()) {
+                    case R.id.action_home:
+                        fragment = new HomeFragment();
+                        Toast.makeText(MainActivity.this, "HOME", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_search:
+                        fragment = new SearchFragment();
+                        Toast.makeText(MainActivity.this, "SEARCH", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_profile:
+                    default:
+                        fragment = new ProfileFragment();
+                        Toast.makeText(MainActivity.this, "PROFILE", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                fragmentManager.beginTransaction().replace(R.id.fragmentContainer, fragment).commit();
+                return true;
+            }
+        });
+        // set default selection
+        bottomNavigationView.setSelectedItemId(R.id.action_home);
     }
 }
