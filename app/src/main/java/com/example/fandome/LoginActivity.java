32276d53445898437d5c337379f98e844443f227
Adaya Head <ahead9@my.westga.edu>
2020-12-04 05:40:45 -0500

    95 percent completed both login and register screen. Currently working on editing the profile fragment screen xml

--- app/src/main/java/com/example/fandome/LoginActivity.java
+++ app/src/main/java/com/example/fandome/LoginActivity.java
@@ -2,15 +2,113 @@ package com.example.fandome;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.fandome.activities.MainActivity;
+import com.example.fandome.fragments.HomeFragment;
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+
 /*
  * A login screen that offers login via username or email/password
  */
 public class LoginActivity extends AppCompatActivity {
 
+    public static final String TAG = "LoginActivity";
+    private EditText usernameInputBox;
+    private EditText passwordInputBox;
+    private Button loginButton;
+    private Button registerButton;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+    protected void onCreate(Bundle saveInstanceState){
+        super.onCreate(saveInstanceState);
+//        setContentView(R.layout.activity_login);
+        setContentView(R.layout.fragment_profile);
+
+
+        //checks if user has previously logged on so they can go straight to main activity
+        if (ParseUser.getCurrentUser() != null){
+            goMainActivity();
+        }
+
+        usernameInputBox = findViewById(R.id.usernameInputBox);
+        passwordInputBox = findViewById(R.id.passwordInputBox);
+        loginButton = findViewById(R.id.signUpButton);
+        registerButton = findViewById(R.id.registerButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "login button clicked!");
+                String username = usernameInputBox.getText().toString();
+                String password = passwordInputBox.getText().toString();
+
+                loginUser(username, password);
+                // TODO: also allow user to log in via email and password
+
+                if (username.isEmpty() && password.isEmpty()){
+                    Toast.makeText(LoginActivity.this, "Please log in with a valid username and password. (Username and password cannot be empty)", Toast.LENGTH_SHORT).show();
+                } else if(username.isEmpty()){
+                    Toast.makeText(LoginActivity.this, "Please log in with a valid username. (Username cannot be empty)", Toast.LENGTH_SHORT).show();
+                } else if(password.isEmpty()){
+                    Toast.makeText(LoginActivity.this, "Please log in with a valid password. (Password cannot be empty)", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "register button clicked!");
+                goRegisterActivity();
+            }
+        });
     }
+
+    //This is where we let our user login in depending on if their login credentials are correct
+    private void loginUser(String username, String password) {
+        Log.i(TAG, "attempting to log in user " + username);
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            //checking if logging in was an issue and if so then it has an error that we check for
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if (e != null){
+                    Log.e(TAG, "Issue with login", e);
+                    return;
+                }
+                //navigates to main activity if the user successfully signs in properly
+                goMainActivity();
+                //Toasts visually show the user that they were able to log in with message @ bottom of screen
+                Toast.makeText(LoginActivity.this, "Hacker voice: I'm in", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    //entering the main activity page
+    private void goMainActivity() {
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+
+        //removes the chance to press the back button and go into the log in page again
+        finish();
+    }
+
+    //entering the register activity page
+    private void goRegisterActivity() {
+        Intent i = new Intent(this, RegisterActivity.class);
+        startActivity(i);
+
+        finish();
+    }
+
 }
--- app/src/main/java/com/example/fandome/RegisterActivity.java
+++ app/src/main/java/com/example/fandome/RegisterActivity.java
@@ -2,15 +2,130 @@ package com.example.fandome;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.fandome.activities.MainActivity;
+import com.example.fandome.fragments.HomeFragment;
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+
 /*
  * A register screen that offers user registration through user's provided information
  */
 public class RegisterActivity extends AppCompatActivity {
 
+    public static final String TAG = "RegisterActivity";
+    private EditText firstNameInputBox;
+    private EditText lastNameInputBox;
+    private EditText usernameInputBox;
+    private EditText passwordInputBox;
+    private EditText emailInputBox;
+    private Button signUpButton;
+    private TextView logInText;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void onCreate(Bundle saveInstanceState){
+        super.onCreate(saveInstanceState);
         setContentView(R.layout.activity_register);
+
+        firstNameInputBox = findViewById(R.id.firstNameInputBox);
+        lastNameInputBox = findViewById(R.id.lastNameInputBox);
+        usernameInputBox = findViewById(R.id.usernameInputBox);
+        passwordInputBox = findViewById(R.id.passwordInputBox);
+        emailInputBox = findViewById(R.id.emailInputBox);
+        logInText = findViewById(R.id.logInText);
+
+        signUpButton = findViewById(R.id.signUpButton);
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "signin button clicked!");
+
+                String firstName = firstNameInputBox.getText().toString();
+                String lastName = lastNameInputBox.getText().toString();
+                String username = usernameInputBox.getText().toString();
+                String password = passwordInputBox.getText().toString();
+                String email = emailInputBox.getText().toString();
+
+                signinUser(username, password);
+
+                if (firstName.isEmpty() && lastName.isEmpty() && username.isEmpty() && password.isEmpty() && email.isEmpty()){
+                    Toast.makeText(RegisterActivity.this, "Please do not leave any inputs empty", Toast.LENGTH_SHORT).show();
+                } else if(firstName.isEmpty()) {
+                    Toast.makeText(RegisterActivity.this, "Please log in with a valid first name. (The first name cannot be empty)", Toast.LENGTH_SHORT).show();
+                } else if(lastName.isEmpty()) {
+                    Toast.makeText(RegisterActivity.this, "Please log in with a valid last name. (The last name cannot be empty)", Toast.LENGTH_SHORT).show();
+                } else if(username.isEmpty()){
+                    Toast.makeText(RegisterActivity.this, "Please log in with a valid username. (Username cannot be empty)", Toast.LENGTH_SHORT).show();
+                } else if(password.isEmpty()){
+                    Toast.makeText(RegisterActivity.this, "Please log in with a valid password. (Password cannot be empty)", Toast.LENGTH_SHORT).show();
+                } else if(email.isEmpty()){
+                    Toast.makeText(RegisterActivity.this, "Please log in with a valid email. (Email cannot be empty)", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+        logInText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "log in text clicked!");
+                goLoginActivity();
+            }
+        });
+
     }
+
+    //This is where we let our user login in depending on if their login credentials are correct
+    private void signinUser(String username, String password){
+        String email = emailInputBox.getText().toString();
+
+        ParseUser user = new ParseUser();
+        user.setUsername(username);
+        user.setPassword(password);
+        user.setEmail(email);
+        Log.i(TAG, "attempting to log in user " + username);
+        user.signUpInBackground(new SignUpCallback() {
+            //checking if logging in was an issue and if so then it has an error that we check for
+            public void done(ParseException e) {
+                if (e != null){
+                    Log.e(TAG, "Issue with login", e);
+                    return;
+                }
+                //navigates to main activity if the user successfully signs in properly
+                goMainActivity();
+                //Toasts visually show the user that they were able to log in with message @ bottom of screen
+                Toast.makeText(RegisterActivity.this, "Hacker voice: I'm in", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    //entering the main activity page
+    private void goMainActivity() {
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+
+        finish();
+    }
+
+    //entering the login activity page
+    private void goLoginActivity() {
+        Intent i = new Intent(this, LoginActivity.class);
+        startActivity(i);
+
+        finish();
+    }
+
+
 }
